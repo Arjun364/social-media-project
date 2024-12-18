474c033ed4b4b40d44d74e0f170bc182b3a12223
Arjun AV <avarjun360@gmail.com>
2024-12-19 02:38:46 +0530

    changed the folder name

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 // 1 load the .env file : it is given first to load it first since the authincation has to be done btw the server and the clinet berfore any thing
 require('dotenv').config();
-const path = require('path');
 // import the express framwork
 const express = require('express')
 // import the router
@@ -10,13 +9,7 @@ const cors =require('cors')
 // create a server using express
 const server = express()
 // middlewares for the server
-server.use(cors({
-    origin:[
-        'http://localhost:5173',
-        'https://social-media-project-fz2u.onrender.com'
-    ]
-
-}))
+server.use(cors())
 server.use(express.json())
 server.use(router)
 // create a port for the server to run :4000
@@ -24,7 +17,7 @@ const PORT = process.env.PORT || 4000
 // import the mongodb connection to the server
 require('./DB/connections')
 // Serve static files from the 'uploads' folder
-server.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+server.use('/uploads', express.static('uploads'));
 
 // default API port
 server.get('/',(req,res)=>{
